925cedd021f817bfcc85a17a83879f69b25486fd
chaimaekarimpro <chaimaekarim.pro@gmail.com>
2026-03-11 10:40:13 +0000

    supprimer

--- resources/views/admin/stats.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title','Statistiques globales')
-
-@section('content')
-<h1 class="text-2xl font-bold mb-4">Statistiques globales</h1>
-<p class="text-sm">Graphiques de participation, taux de réussite, etc.</p>
-@endsection
--- resources/views/teacher/results.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title','Résultats élèves')
-
-@section('content')
-<h1 class="text-2xl font-bold mb-4">Résultats des élèves</h1>
-<p class="text-sm">Affichage des résultats par challenge et par élève.</p>
-@endsection
